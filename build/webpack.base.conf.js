233614f86f35575ea181c8862b57bbcf6ff733ef
Stefan Peters <stefandesu@exo.pm>
2018-05-08 08:29:19 +0200

    Add BootstrapVue to white-list in Webpack config.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -39,7 +39,12 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [
+          resolve('src'),
+          resolve('test'),
+          resolve('node_modules/webpack-dev-server/client'),
+          resolve("node_modules/bootstrap-vue")
+        ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
