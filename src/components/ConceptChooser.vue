0c3ce7609b834a42a771254f55de279048a6a611
Stefan Peters <stefandesu@exo.pm>
2018-05-08 13:42:54 +0200

    Adjust display of parent and children concepts
    Use links instead of buttons with variant link because links allow long lines to break into the next line.
    Display dots on parent concepts to show depth.
    Display arrows on child concepts to make navigation clearer.

--- src/components/ConceptChooser.vue
+++ src/components/ConceptChooser.vue
@@ -2,30 +2,35 @@
   <div>
     <span v-if="concept == null">
       <strong>Top Concepts</strong>
-      <ul v-show="!loading">
+      <ul class="concept" v-show="!loading">
         <li v-for="(d, i) in tops" :key="i">
-          <b-button variant="link" @click="choose(d, 0)">{{ d.prefLabel.de }}</b-button>
+          <b-link @click="choose(d, 0)">{{ d.prefLabel.de }}</b-link>
         </li>
       </ul>
       <loading-indicator v-show="loading" />
     </span>
     <span v-else>
       <!-- 1. List of parent concepts including link to Top Concepts -->
-      <ul>
+      <ul class="concept conceptSmall">
         <li>
-          <b-button variant="link" size="sm" @click="concept = null">Top Concepts</b-button>
+          <b-link @click="concept = null">Top Concepts</b-link>
         </li>
         <li v-for="(p, i) in parents" :key="i">
-          <b-button variant="link" size="sm" @click="choose(p, i)">{{ p.prefLabel.de }}</b-button>
+          <b-link @click="choose(p, i)">
+            <span v-for="j in i + 1" :key="j">•</span>
+            {{ p.prefLabel.de }}
+          </b-link>
         </li>
       </ul>
       <!-- 2. Currently chosen concept and notation -->
-      <b-badge>{{ concept.notation ? concept.notation[0] : '?' }}</b-badge> <strong>{{ concept.prefLabel.de }}</strong>
+      <div>
+        <b-badge>{{ concept.notation ? concept.notation[0] : '?' }}</b-badge> <strong>{{ concept.prefLabel.de }}</strong>
+      </div>
       <!-- 3. List of child concepts -->
       <loading-indicator v-show="loading" />
-      <ul>
+      <ul class="concept conceptSmall">
         <li v-for="(c, i) in children" :key="i">
-          <b-button variant="link" size="sm" @click="choose(c, depth+1)">{{ c.prefLabel.de }}</b-button>
+          <b-link @click="choose(c, depth+1)">↪ {{ c.prefLabel.de }}</b-link>
         </li>
       </ul>
     </span>
@@ -125,15 +130,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-ul {
+.concept {
+  font-size: 1em;
   list-style-type: none;
   padding: 0;
   margin-bottom: 10px;
-  margin-top: 6px;
+  margin-top: 10px;
+  line-height: 1.2;
 }
-li {
-  margin-left: 5px;
-  margin-top: 0px;
-  margin-bottom: -5px;
+.concept > li {
+  margin-left: 20px;
+  margin-top: 10px;
+  margin-bottom: 5px;
+}
+.conceptSmall {
+  font-size: 0.9em;
 }
 </style>
