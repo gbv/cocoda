c479e4e7534931e8c2b72623edb714b56ece95cf
Stefan Peters <stefandesu@exo.pm>
2018-05-16 12:13:41 +0200

    Add keyboard navigation to SearchField

--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -6,12 +6,20 @@
       type="search"
       placeholder="Type to search..."
       @click.native="isOpen = searchQuery != ''"
-      @change="isOpen = searchQuery != ''"></b-form-input>
-    <ul id="searchfield-results" v-show="isOpen" class="searchfield-results">
+      @keydown.down.native.prevent="onArrowDown"
+      @keydown.up.native.prevent="onArrowUp"
+      @keyup.enter.native="onEnter"></b-form-input>
+    <ul v-show="isOpen" class="searchfield-results">
       <li class="loading" v-if="loading">
         <loading-indicator />
       </li>
-      <li v-else v-for="(result, i) in searchResult" :key="i" class="searchfield-result" @click="chooseResult(result)">
+      <li v-else v-for="(result, i) in searchResult"
+        :key="i"
+        class="searchfield-result"
+        :id="'searchResult' + i"
+        @click="chooseResult(result)"
+        @mouseover="mouseover(i)"
+        :class="{ 'searchfield-selected': i === searchSelected }">
         {{ result[0] }}
       </li>
     </ul><br>
@@ -22,6 +30,20 @@
 import * as api from './api'
 import LoadingIndicator from './LoadingIndicator'
 var _ = require('lodash')
+
+/**
+ * Helper function  that scrolls the search result when navigating with the keyboard
+ */
+function scrollIntoViewIfNeeded(target) {
+  var rect = target.getBoundingClientRect();
+  var parentRect = target.parentElement.getBoundingClientRect()
+  if (rect.bottom > parentRect.bottom) {
+    target.scrollIntoView(false);
+  }
+  if (rect.top < parentRect.top) {
+    target.scrollIntoView();
+  }
+}
 
 export default {
   name: 'SearchField',
@@ -34,12 +56,14 @@ export default {
       searchQuery: "",
       searchResult: [],
       isOpen: false,
-      loading: false
+      loading: false,
+      searchSelected: -1
     }
   },
   watch: {
     // whenever question changes, this function will run
     searchQuery: function (newQuestion, oldQuestion) {
+      this.searchSelected = -1
       if (newQuestion == "") {
         this.loading = false
         this.isOpen = false
@@ -69,6 +93,7 @@ export default {
       this.$emit('chooseUri', uri)
       this.searchQuery = ""
       this.isOpen = false
+      this.searchSelected = -1
     },
     getAnswer:  function () {
       this.searchResult = ['Searching...']
@@ -76,7 +101,6 @@ export default {
       api.suggest(this.searchQuery, this.voc, 20)
         .then(function(data) {
           vm.loading = false
-          vm.isOpen = true
           vm.searchResult = _.zip(data[1], data[2], data[3])
         })
         .catch(function(error) {
@@ -87,7 +111,41 @@ export default {
     handleClickOutside(evt) {
       if (!this.$el.contains(evt.target)) {
         this.isOpen = false;
+        this.searchSelected = -1
       }
+    },
+    onArrowDown() {
+      if (this.searchSelected >= this.searchResult.length - 1) {
+        this.searchSelected = 0
+      } else {
+        this.searchSelected += 1
+      }
+      this.scrollSelectedIntoView()
+    },
+    onArrowUp() {
+      if (this.searchSelected <= 0) {
+        this.searchSelected = this.searchResult.length - 1
+      } else {
+        this.searchSelected -= 1
+      }
+      this.scrollSelectedIntoView()
+    },
+    scrollSelectedIntoView(alignTo=null) {
+      scrollIntoViewIfNeeded(document.getElementById('searchResult'+this.searchSelected))
+    },
+    onEnter() {
+      let chosenIndex
+      if (this.searchResult.length == 0) {
+        return
+      } else if (this.searchSelected < 0 || this.searchSelected >= this.searchResult.length) {
+        chosenIndex = 0
+      } else {
+        chosenIndex = this.searchSelected
+      }
+      this.chooseResult(this.searchResult[chosenIndex])
+    },
+    mouseover(i) {
+      this.searchSelected = i
     }
   },
   mounted() {
@@ -126,7 +184,7 @@ export default {
   padding-left: 8px;
 }
 
-.searchfield-result:hover {
+.searchfield-selected {
   background-color: #4AAE9B;
   color: white;
 }
