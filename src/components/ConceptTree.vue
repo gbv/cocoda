ae4b13dc60c17832f82cd2fceded9d8d3426b98d
Stefan Peters <stefandesu@exo.pm>
2018-05-17 11:25:04 +0200

    Add first version of new ConceptTree
    Issue #11 (work in progress)

---
+++ src/components/ConceptTree.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="conceptTree">
+    <concept-tree-item
+      v-for="(concept, index) in tree"
+      :key="index"
+      :concept="concept"
+      :selected="selected"
+      :hovered="hovered"
+      :depth="0"
+      :index="index"
+      @hovered="hovered = $event"
+      @selected="selected = $event"
+      @update="update($event)" />
+  </div>
+</template>
+
+<script>
+import * as api from './api'
+import LoadingIndicator from './LoadingIndicator'
+import ConceptTreeItem from './ConceptTreeItem'
+
+// Helper function to sort data. Sort by notation if possible, otherwise by uri.
+function sortData(data) {
+  return data.sort(
+    (a,b) => a.notation && b.notation ? a.notation[0] > b.notation[0] : a.uri > b.uri
+  )
+}
+
+export default {
+  name: 'concepttree',
+  components: {
+    LoadingIndicator, ConceptTreeItem
+  },
+  props: ['vocSelected'],
+  data () {
+    return {
+      tree: [],
+      selected: null,
+      loading: false,
+      hovered: null
+    }
+  },
+  watch: {
+    vocSelected: function(newValue, oldValue) {
+      if (oldValue != newValue) {
+        this.reset()
+      }
+    },
+    selected: function(newValue, oldValue) {
+      this.$emit('selectedConcept', newValue != null ? newValue.uri : null)
+    }
+  },
+  methods: {
+    chooseFromUri: function(uri) {
+      // ...
+    },
+    reset: function() {
+      this.tree = []
+      this.selected = null
+      this.hovered = null
+      this.loading = true
+      let selectedBefore = this.vocSelected
+      let vm = this
+      api.topByNotation(this.vocSelected.notation[0])
+        .then(function(data) {
+          if (selectedBefore != vm.vocSelected) {
+            console.log('Another voc was chosen in the meanwhile.')
+          } else {
+            // Save data sorted by uri
+            let tree = sortData(data)
+            for (var i = 0; i < tree.length; i++) {
+              tree[i].isOpen = false
+            }
+            vm.tree = tree
+            vm.loading = false
+          }
+        }).catch(function(error) {
+          console.log('Request failed', error)
+          vm.loading = false
+        })
+    },
+    update(concept) {
+      // Recursively search for concept by uri and update it
+      let path = this.tree.pathForUri(concept.uri, true)
+      if (path != null) {
+        let lastIndex = path.pop()
+        let element = this.tree
+        for (var index of path) {
+          element = element[index].narrower
+        }
+        element[index] = concept
+      } else {
+        console.log("Could not determine path for uri", concept.uri)
+      }
+    }
+  }
+}
+
+// Add a prototype method to Array to find path for uri
+Array.prototype.pathForUri = function(uri, recursive = false, path = []) {
+  let shouldReturn = false
+  let newPath = path
+  this.forEach(function(concept, index) {
+    if (!shouldReturn && concept != null && concept.uri == uri) {
+      newPath.push(index)
+      shouldReturn = true
+    }
+  })
+  if (recursive) {
+    this.forEach(function(concept, index) {
+      if (shouldReturn || concept == null || !concept.narrower) {
+        return
+      }
+      let result = concept.narrower.pathForUri(uri, true, newPath.concat([index]))
+      if (result != null) {
+        newPath = result
+        shouldReturn = true
+      }
+    })
+  }
+  if (shouldReturn) {
+    return newPath
+  } else {
+    return null
+  }
+}
+
+</script>
+
+<style scoped>
+.conceptTree {
+  flex: 1.5;
+  overflow-y: auto;
+}
+.concept {
+  font-size: 1em;
+  list-style-type: none;
+  padding: 0;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  line-height: 1.2;
+}
+.concept > li {
+  margin-left: 20px;
+  margin-top: 10px;
+  margin-bottom: 5px;
+}
+.conceptSmall {
+  font-size: 0.9em;
+}
+</style>
---
+++ src/components/ConceptTreeItem.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="conceptTreeItem" v-if="concept != null" :style="{ 'margin-left': depth * 5 + 'px' }">
+    <div
+      class="conceptBox"
+      :class="{
+        conceptBoxHovered: isHovered,
+        conceptBoxSelected: isSelected
+      }">
+      <div class="arrowBox" @click="open(!isOpen)" v-if="hasChildren"><i :class="{ right: !isOpen, down: isOpen }"></i></div>
+      <div
+        class="labelBox"
+        @mouseover="hovering(concept)"
+        @mouseout="hovering(null)"
+        @click="onClick"
+        :class="{ labelBoxFull: !hasChildren }">
+        <item-name :item="concept" />
+      </div>
+    </div>
+    <div class="conceptChildrenBox" v-if="isOpen">
+      <concept-tree-item
+        v-for="(child, index) in concept.narrower"
+        :key="index"
+        :concept="child"
+        :selected="selected"
+        :hovered="hovered"
+        :depth="depth + 1"
+        :index="index"
+        @hovered="hovering($event)"
+        @selected="select($event)" />
+    </div>
+    <loading-indicator v-show="loadingChildren" />
+  </div>
+</template>
+
+<script>
+import * as api from './api'
+import LoadingIndicator from './LoadingIndicator'
+import ItemName from './ItemName'
+
+// Helper function to sort data. Sort by notation if possible, otherwise by uri.
+function sortData(data) {
+  return data.sort(
+    (a,b) => a.notation && b.notation ? a.notation[0] > b.notation[0] : a.uri > b.uri
+  )
+}
+
+export default {
+  name: 'concept-tree-item',
+  components: {
+    LoadingIndicator, ItemName
+  },
+  props: ['concept', 'selected', 'hovered', 'depth', 'index'],
+  data () {
+    return {
+      loadingChildren: false
+    }
+  },
+  computed: {
+    hasChildren() {
+      return !this.concept.narrower || this.concept.narrower.length != 0
+    },
+    isOpen() {
+      return this.concept.isOpen ? true : false
+    },
+    isHovered() {
+      return this.hovered != null ? this.hovered.uri == this.concept.uri : false
+    },
+    isSelected() {
+      return this.selected != null ? this.selected.uri == this.concept.uri : false
+    }
+  },
+  watch: {},
+  methods: {
+    hovering(el) {
+      this.$emit('hovered', el)
+    },
+    update(concept) {
+      this.$emit('update', concept)
+    },
+    open(isOpen) {
+      let newConcept = this.concept
+      newConcept.isOpen = isOpen
+      this.update(newConcept)
+      this.loadChildren()
+    },
+    select(concept) {
+      this.$emit('selected', concept)
+    },
+    onClick() {
+      if (!this.isSelected) {
+        this.select(this.concept)
+      } else {
+        this.open(!this.isOpen)
+      }
+    },
+    loadChildren() {
+      if (!this.hasChildren) {
+        return
+      }
+      let children = []
+      let vm = this
+      if (this.loadingChildren) {
+        // Possibly cancel token
+      } else {
+        this.loadingChildren = true
+      }
+      // Generate new cancel token
+      // this.cancelTokenChildren = axios.CancelToken.source()
+      api.narrower(this.concept.uri, api.defaultProperties)
+        .then(function(data) {
+          vm.loadingChildren = false
+          let newConcept = vm.concept
+          let children = sortData(data)
+          for (var i = 0; i < children.length; i++) {
+            children[i].isOpen = false
+          }
+          newConcept.narrower = children
+          vm.update(newConcept)
+        }).catch(function(error) {
+          console.log('Request failed', error)
+          vm.loadingChildren = false
+        })
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.conceptBox {
+  display: flex;
+  min-height: 24px;
+  margin: 3px;
+  cursor: pointer;
+  user-select: none;
+}
+.arrowBox {
+  flex: none;
+  width: 18px;
+  padding-left: 4px;
+}
+.labelBox {
+  flex: 1;
+  vertical-align: center;
+}
+.labelBoxFull {
+  padding-left: 18px;
+}
+.conceptBoxHovered, .arrowBox:hover {
+  background-color: bisque;
+}
+.conceptBoxSelected {
+  font-weight: bold;
+}
+/* For arrows, from https://www.w3schools.com/howto/howto_css_arrows.asp */
+i {
+  border: solid black;
+  border-width: 0 2px 2px 0;
+  display: inline-block;
+  padding: 3px;
+}
+.right {
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+}
+.left {
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+}
+.up {
+  transform: rotate(-135deg);
+  -webkit-transform: rotate(-135deg);
+}
+.down {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
+</style>
