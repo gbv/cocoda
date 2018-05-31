23d72ba84f17a36d4dff3474dd6e2a1665ff371a
Stefan Peters <stefandesu@exo.pm>
2018-05-31 09:36:19 +0200

    Add selected concepts to occurrences table

--- src/components/MappingOccurrences.vue
+++ src/components/MappingOccurrences.vue
@@ -8,7 +8,6 @@
         :items="items"
         :fields="fields"
         class="defaultTable"
-        striped
         small
         thead-class="defaultTableHead"
         tbody-class="defaultTableBody" />
@@ -51,25 +50,58 @@ export default {
   computed: {
     items() {
       let items = []
-      for (let concept of this.mapping.jskos.from.memberSet) {
-        if (concept.OCCURRENCES == null) {
-          this.loadOccurrences(concept)
-        }
-        items.push({
-          from: concept.notation[0],
-          to: "-",
-          occurrences: this.occurrencesToString(concept.OCCURRENCES)
+      let intermediateItems = []
+      // Add selected concepts
+      // TODO: - Scheme for selected concept might differ from scheme for mapping.
+      if (!this.mapping.reversed) {
+        intermediateItems.push({
+          concept: this.selectedLeft,
+          type: "from",
+          _rowVariant: "info"
+        }, {
+          concept: this.selectedRight,
+          type: "to",
+          _rowVariant: "success"
+        })
+      } else {
+        intermediateItems.push({
+          concept: this.selectedRight,
+          type: "from",
+          _rowVariant: "success"
+        }, {
+          concept: this.selectedLeft,
+          type: "to",
+          _rowVariant: "info"
         })
       }
+      for (let concept of this.mapping.jskos.from.memberSet) {
+        if ((!this.selectedLeft || this.selectedLeft.uri != concept.uri) && (!this.selectedRight || this.selectedRight.uri != concept.uri)) {
+          intermediateItems.push({
+            concept: concept,
+            type: "from"
+          })
+        }
+      }
       for (let concept of this.mapping.jskos.to.memberSet) {
-        if (concept.OCCURRENCES == null) {
-          this.loadOccurrences(concept)
+        if ((!this.selectedLeft || this.selectedLeft.uri != concept.uri) && (!this.selectedRight || this.selectedRight.uri != concept.uri)) {
+          intermediateItems.push({
+            concept: concept,
+            type: "to"
+          })
+        }
+      }
+      for (let item of intermediateItems) {
+        if (item.concept == null) {
+          continue
+        }
+        if (item.concept.OCCURRENCES == null) {
+          this.loadOccurrences(item.concept)
         }
         items.push({
-          from: "-",
-          to: concept.notation[0],
-          occurrences: this.occurrencesToString(concept.OCCURRENCES)
+          occurrences: this.occurrencesToString(item.concept.OCCURRENCES),
+          _rowVariant: item._rowVariant ? item._rowVariant : ""
         })
+        items[items.length-1][item.type] = item.concept.notation[0]
       }
       return items
     },
