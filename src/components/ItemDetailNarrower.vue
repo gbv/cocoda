73af55eb00da31b7443880136ae7d25f1f47a4c2
Stefan Peters <Stefan.Peters@gbv.de>
2018-08-21 08:47:21 +0200

    Fix small issue in ItemDetailNarrower

--- src/components/ItemDetailNarrower.vue
+++ src/components/ItemDetailNarrower.vue
@@ -44,7 +44,7 @@ export default {
      */
     narrower: {
       type: Array,
-      default: new Array([])
+      default: () => new Array([])
     },
     /**
      * The text to be displayed on the top.
