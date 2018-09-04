c86fde0a4af4e5044942d1c3948ccbe84ac39df3
Stefan Peters <Stefan.Peters@gbv.de>
2018-09-04 14:26:32 +0200

    Implement concordances browser in MappingsApp

--- src/MappingsApp.vue
+++ src/MappingsApp.vue
@@ -17,145 +17,186 @@
     </div>
     <loading-indicator-full v-if="loading" />
     <div id="main">
-      <h2>Search Mappings</h2>
-      <b-container class="search">
-        <b-row>
-          <b-col
-            cols="2"
-            class="label">Source</b-col>
-          <b-col cols="3">
-            <b-form-input
-              v-model="sourceScheme"
-              :state="sourceScheme == '' ? null : fromScheme != null"
-              type="text"
-              placeholder="scheme"
-              @keyup.enter.native="searchClicked" />
-          </b-col>
-          <b-col cols="5">
-            <b-form-input
-              v-model="sourceNotation"
-              type="text"
-              placeholder="notation"
-              @keyup.enter.native="searchClicked" />
-          </b-col>
-          <b-col cols="2">
-            <b-button
-              variant="outline-primary"
-              @click="swapClicked" >
-              <!-- <font-awesome-icon icon="retweet" /> -->
-              <font-awesome-icon icon="long-arrow-alt-down" />
-              <font-awesome-icon icon="long-arrow-alt-up" />
-            </b-button>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col
-            cols="2"
-            class="label">Target</b-col>
-          <b-col cols="3">
-            <b-form-input
-              v-model="targetScheme"
-              :state="targetScheme == '' ? null : toScheme != null"
-              type="text"
-              placeholder="scheme"
-              @keyup.enter.native="searchClicked" />
-          </b-col>
-          <b-col cols="5">
-            <b-form-input
-              v-model="targetNotation"
-              type="text"
-              placeholder="notation"
-              @keyup.enter.native="searchClicked" />
-          </b-col>
-          <b-col cols="2" />
-        </b-row>
-        <b-row>
-          <b-col
-            cols="2"
-            class="label">Creator</b-col>
-          <b-col cols="3">
-            <b-form-input
-              v-model="creator"
-              type="text"
-              placeholder="creator"
-              @keyup.enter.native="searchClicked" />
-          </b-col>
-          <b-col
-            cols="1"
-            class="label">Type</b-col>
-          <b-col cols="4">
-            <b-select
-              v-model="type"
-              :options="typeOptions" />
-          </b-col>
-          <b-col cols="2" />
-        </b-row>
-        <b-row>
-          <b-col cols="2" />
-          <b-col cols="2">
-            <b-button
-              variant="danger"
-              @click="clearClicked" >
-              <font-awesome-icon icon="ban" /> Clear
-            </b-button>
-          </b-col>
-          <b-col cols="4" />
-          <b-col
-            class="text-right"
-            cols="2" >
-            <b-button
-              variant="primary"
-              @click="searchClicked" >
-              <font-awesome-icon icon="search" /> Search
-            </b-button>
-          </b-col>
-          <b-col cols="2" />
-        </b-row>
-      </b-container>
-      <b-container
-        :class="{
-          mappingTable: true,
-          'mappingTable-short': mappings.length <= 2
-        }"
-      >
-        <b-row v-if="totalCount > 0">
-          <b-col
-            cols="12" >
-            <b-pagination
-              v-model="page"
-              :total-rows="totalCount"
-              :per-page="10"
-              class="justify-content-center"
-              style="user-select: none;"
-              size="sm" />
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col cols="12">
-            <mapping-table
-              v-show="mappings.length"
-              :mappings="mappings"
-              :actions="actions"
-              :hide-duplicates="false"
-              @click="tableClicked" />
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col
-            cols="12"
-            class="text-center" >
-            <p>{{ totalCount }} results</p>
-            <p v-if="downloadUrl && totalCount > 0">
-              <a :href="downloadUrl + 'json'">
-                <font-awesome-icon icon="download" /> .json
-              </a> |
-              <a :href="downloadUrl + 'ndjson'">
-                <font-awesome-icon icon="download" /> .ndjson
-              </a>
+      <b-card no-body>
+        <b-tabs
+          v-model="tab"
+          card >
+          <b-tab
+            title="Concordances">
+            <h2>Concordances</h2>
+            <flexible-table
+              :fields="concordanceTableFields"
+              :items="concordanceTableItems" >
+              <span
+                slot="download"
+                slot-scope="{ value }" >
+                <span
+                  v-for="(distribution, index) in value"
+                  :key="index" >
+                  <a
+                    v-if="nameOfDistribution(distribution)"
+                    :href="distribution.download" >
+                    {{ nameOfDistribution(distribution) }}
+                </a></span>
+              </span>
+              <span
+                slot="mappings"
+                slot-scope="{ value }" >
+                {{ (parseInt(value) || "?").toLocaleString() }}
+              </span>
+            </flexible-table>
+            <p style="text-align: right; font-weight: bold;">
+              Total: {{ concordances.reduce((total, current) => {
+                return total + parseInt(current.extent) || 0
+              }, 0).toLocaleString() }}
             </p>
-          </b-col>
-        </b-row>
-      </b-container>
+            <p>
+              The list of concordances is <a :href="`${concordancesUrl}?download=ndjson`">also available in JSKOS format</a>.
+            </p>
+          </b-tab>
+          <b-tab title="Mappings">
+            <h2>Search Mappings</h2>
+            <b-container class="search">
+              <b-row>
+                <b-col
+                  cols="2"
+                  class="label">Source</b-col>
+                <b-col cols="3">
+                  <b-form-input
+                    v-model="sourceScheme"
+                    :state="sourceScheme == '' ? null : fromScheme != null"
+                    type="text"
+                    placeholder="scheme"
+                    @keyup.enter.native="searchClicked" />
+                </b-col>
+                <b-col cols="5">
+                  <b-form-input
+                    v-model="sourceNotation"
+                    type="text"
+                    placeholder="notation"
+                    @keyup.enter.native="searchClicked" />
+                </b-col>
+                <b-col cols="2">
+                  <b-button
+                    variant="outline-primary"
+                    @click="swapClicked" >
+                    <!-- <font-awesome-icon icon="retweet" /> -->
+                    <font-awesome-icon icon="long-arrow-alt-down" />
+                    <font-awesome-icon icon="long-arrow-alt-up" />
+                  </b-button>
+                </b-col>
+              </b-row>
+              <b-row>
+                <b-col
+                  cols="2"
+                  class="label">Target</b-col>
+                <b-col cols="3">
+                  <b-form-input
+                    v-model="targetScheme"
+                    :state="targetScheme == '' ? null : toScheme != null"
+                    type="text"
+                    placeholder="scheme"
+                    @keyup.enter.native="searchClicked" />
+                </b-col>
+                <b-col cols="5">
+                  <b-form-input
+                    v-model="targetNotation"
+                    type="text"
+                    placeholder="notation"
+                    @keyup.enter.native="searchClicked" />
+                </b-col>
+                <b-col cols="2" />
+              </b-row>
+              <b-row>
+                <b-col
+                  cols="2"
+                  class="label">Creator</b-col>
+                <b-col cols="3">
+                  <b-form-input
+                    v-model="creator"
+                    type="text"
+                    placeholder="creator"
+                    @keyup.enter.native="searchClicked" />
+                </b-col>
+                <b-col
+                  cols="1"
+                  class="label">Type</b-col>
+                <b-col cols="4">
+                  <b-select
+                    v-model="type"
+                    :options="typeOptions" />
+                </b-col>
+                <b-col cols="2" />
+              </b-row>
+              <b-row>
+                <b-col cols="2" />
+                <b-col cols="2">
+                  <b-button
+                    variant="danger"
+                    @click="clearClicked" >
+                    <font-awesome-icon icon="ban" /> Clear
+                  </b-button>
+                </b-col>
+                <b-col cols="4" />
+                <b-col
+                  class="text-right"
+                  cols="2" >
+                  <b-button
+                    variant="primary"
+                    @click="searchClicked" >
+                    <font-awesome-icon icon="search" /> Search
+                  </b-button>
+                </b-col>
+                <b-col cols="2" />
+              </b-row>
+            </b-container>
+            <b-container
+              :class="{
+                mappingTable: true,
+                'mappingTable-short': mappings.length <= 2
+              }"
+            >
+              <b-row v-if="totalCount > 0">
+                <b-col
+                  cols="12" >
+                  <b-pagination
+                    v-model="page"
+                    :total-rows="totalCount"
+                    :per-page="10"
+                    class="justify-content-center"
+                    style="user-select: none;"
+                    size="sm" />
+                </b-col>
+              </b-row>
+              <b-row>
+                <b-col cols="12">
+                  <mapping-table
+                    v-show="mappings.length"
+                    :mappings="mappings"
+                    :actions="actions"
+                    :hide-duplicates="false"
+                    @click="tableClicked" />
+                </b-col>
+              </b-row>
+              <b-row>
+                <b-col
+                  cols="12"
+                  class="text-center" >
+                  <p>{{ totalCount }} results</p>
+                  <p v-if="downloadUrl && totalCount > 0">
+                    <a :href="downloadUrl + 'json'">
+                      <font-awesome-icon icon="download" /> .json
+                    </a> |
+                    <a :href="downloadUrl + 'ndjson'">
+                      <font-awesome-icon icon="download" /> .ndjson
+                    </a>
+                  </p>
+                </b-col>
+              </b-row>
+            </b-container>
+          </b-tab>
+        </b-tabs>
+      </b-card>
     </div>
   </div>
 </template>
@@ -163,6 +204,7 @@
 <script>
 import axios from "axios"
 import MappingTable from "./components/MappingTable"
+import FlexibleTable from "vue-flexible-table"
 import _ from "lodash"
 import LoadingIndicatorFull from "./components/LoadingIndicatorFull"
 
@@ -172,7 +214,7 @@ import LoadingIndicatorFull from "./components/LoadingIndicatorFull"
 export default {
   name: "MappingsApp",
   components: {
-    MappingTable, LoadingIndicatorFull
+    MappingTable, LoadingIndicatorFull, FlexibleTable
   },
   data () {
     return {
@@ -180,7 +222,9 @@ export default {
       loadingId: null,
       schemes: null,
       currentPage: 1,
+      tab: 0,
       downloadUrl: null,
+      concordances: [],
       mappings: [],
       totalCount: 0,
       itemDetailSettings: {
@@ -251,6 +295,76 @@ export default {
         this.currentPage = value
       }
     },
+    concordanceTableFields() {
+      return [
+        {
+          key: "from",
+          width: "10%",
+          minWidth: "",
+          sortable: true,
+          align: "left",
+        },
+        {
+          key: "to",
+          width: "10%",
+          minWidth: "",
+          sortable: true,
+          align: "left",
+        },
+        {
+          key: "description",
+          width: "34%",
+          minWidth: "",
+          sortable: true,
+          align: "left",
+        },
+
+        {
+          key: "creator",
+          width: "20%",
+          minWidth: "",
+          sortable: true,
+          align: "left",
+        },
+
+        {
+          key: "download",
+          width: "12%",
+          minWidth: "",
+          sortable: false,
+          align: "left",
+        },
+
+        {
+          key: "mappings",
+          width: "14%",
+          minWidth: "",
+          sortable: true,
+          align: "right",
+          compare: (a, b) => (parseInt(a.mappings) || 0) - (parseInt(b.mappings) || 0),
+        },
+      ]
+    },
+    concordanceTableItems() {
+      let items = []
+      for (let concordance of this.concordances) {
+        let item = { concordance }
+        item.from = _.get(concordance, "fromScheme.notation[0]", "-")
+        item.to = _.get(concordance, "toScheme.notation[0]", "-")
+        item.description = _.get(concordance, "scopeNote.de[0]") || _.get(concordance, "scopeNote.en[0]") || "-"
+        item.creator = _.get(concordance, "creator[0].prefLabel.de") || _.get(concordance, "creator[0].prefLabel.en") || "-"
+        item.download = _.get(concordance, "distributions", [])
+        item.mappings = _.get(concordance, "extent")
+        items.push(item)
+      }
+      return items
+    },
+    concordancesUrl() {
+      // TODO: Fix URLs in config file.
+      let url = this.config.mappingProviders[0].url
+      url = url.substring(0, url.substring(0, url.length - 1).lastIndexOf("/")) + "/concordances"
+      return url
+    }
   },
   watch: {
     schemes() {
@@ -267,6 +381,11 @@ export default {
         this.search()
       }
     },
+    tab() {
+      let query = _.cloneDeep(this.$route.query)
+      query.tab = this.tab
+      this.$router.push({ query })
+    },
   },
   created() {
     // Set loading to true if schemes are not loaded yet.
@@ -279,6 +398,13 @@ export default {
     }).catch(error => {
       console.warn("Error fetching mapping schemes:", error)
       this.schemes = []
+    })
+    // Load concordances from API.
+    axios.get(this.concordancesUrl).then(({ data }) => {
+      this.concordances = data
+    }).catch(error => {
+      console.warn("Error fetching mapping schemes:", error)
+      this.concordances = []
     })
     // Load from parameters on popstate (when using the browser's forward and backward buttons).
     window.addEventListener("popstate", () => {
@@ -296,6 +422,8 @@ export default {
           search = true
         }
       })
+      // Set tab
+      this.tab = parseInt(query.tab) || this.tab || 0
       if (search) {
         // FIXME: saveToParameters will be called even with parameter save = false.
         this.search(false)
@@ -407,6 +535,16 @@ export default {
         }
       }
     },
+    nameOfDistribution(distribution) {
+      let mimetype = distribution.mimetype
+      if (mimetype.includes("json")) {
+        return "JSKOS"
+      }
+      if (mimetype.includes("csv")) {
+        return "CSV"
+      }
+      return null
+    },
   },
 }
 </script>
@@ -441,7 +579,7 @@ html, body {
   text-align: right;
 }
 .mappingTable {
-  padding-bottom: 30px;
+  padding-bottom: 10px;
 }
 .mappingTable .flexibleTable-body {
   padding-bottom: 20px;
