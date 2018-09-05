9e6f21563219dd82d7205061cefa568c37b41c70
Stefan Peters <Stefan.Peters@gbv.de>
2018-09-05 10:53:26 +0200

    Fix issue in webpack.dev.conf introduced in commit adb6b9d

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -56,7 +56,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "index.html",
-      chunks: ["mainApp"],
+      chunks: ["app"],
       inject: true
     }),
     new HtmlWebpackPlugin({
