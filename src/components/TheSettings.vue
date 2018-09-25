59f72ffb5cb498d618a6478547b7bd81e15ff1e0
Stefan Peters <Stefan.Peters@gbv.de>
2018-09-25 08:23:12 +0200

    Add mapping downloads for all concept scheme combinations

--- src/components/TheSettings.vue
+++ src/components/TheSettings.vue
@@ -45,6 +45,25 @@
         target="_blank" >
         Download all {{ dlAllMappings.split("\n").length }} mappings as JSKOS
       </a>
+      <br><br>
+      <span
+        v-for="(download, index) in dlMappings"
+        :key="index">
+        {{ download.label }} ({{ download.mappings.length }}):
+        <a
+          :href="'data:text/json;charset=utf-8,' + download.ndjson"
+          :download="download.filename + '.ndjson'"
+          target="_blank" >
+          JSKOS
+        </a>
+        <a
+          :href="'data:text/json;charset=utf-8,' + download.csv"
+          :download="download.filename + '.csv'"
+          target="_blank" >
+          CSV
+        </a>
+        <br>
+      </span>
     </div>
     <br>
     <p>
@@ -85,6 +104,7 @@ export default {
       localSettings: null,
       creatorRewritten: false,
       dlAllMappings: null,
+      dlMappings: [],
     }
   },
   watch: {
@@ -104,8 +124,45 @@ export default {
       this.$refs.settingsModal.show()
       this.localSettings = _.cloneDeep(this.$settings)
       // Set download data variables
+      this.dlAllMappings = null
+      this.dlMappings = []
       this.$api.getLocalMappings().then(mappings => {
+        // Set all mappings variable
         this.dlAllMappings = mappings.map(mapping => JSON.stringify(this.$jskos.minifyMapping(mapping))).join("\n")
+        // First, determine available combinations of concept schemes
+        for (let mapping of mappings) {
+          let download = this.dlMappings.find(dl => this.$jskos.compare(mapping.fromScheme, dl.fromScheme) && this.$jskos.compare(mapping.toScheme, dl.toScheme))
+          if (download) {
+            download.mappings.push(mapping)
+          } else {
+            download = {
+              fromScheme: mapping.fromScheme,
+              toScheme: mapping.toScheme,
+              mappings: [mapping],
+            }
+            this.dlMappings.push(download)
+          }
+        }
+        // Then, set download property and label of each combination
+        for (let download of this.dlMappings) {
+          // Download as JSKOS/ndjson
+          download.ndjson = download.mappings.map(mapping => JSON.stringify(this.$jskos.minifyMapping(mapping))).join("\n")
+          // Download as CSV
+          let csv = "fromNotation,toNotation,type\n"
+          for (let mapping of download.mappings) {
+            let from = _.get(mapping, "from.memberSet[0].notation[0]")
+            let to = _.get(mapping, "to.memberSet[0].notation[0]")
+            let type = this.$util.mappingTypeByUri(_.get(mapping, "type[0]"))
+            if (from && to && type) {
+              csv += `${from},${to},${type.short}\n`
+            }
+          }
+          download.csv = csv
+          // Label
+          download.label = _.get(download, "fromScheme.notation[0]", "?") + " to " + _.get(download, "toScheme.notation[0]", "?")
+          // Filename
+          download.filename = `${_.get(download, "fromScheme.notation[0]", "?")}_to_${_.get(download, "toScheme.notation[0]", "?")}_${this.localSettings.creator}`
+        }
       })
     },
     rewriteCreator() {
