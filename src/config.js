2874a2611e3b53fa7fbff0e4b81f76f7b438783b
Stefan Peters <Stefan.Peters@gbv.de>
2018-11-20 15:17:16 +0100

    Adjust config format with JSKOS specification (#64)
    The config loading script was adjusted so that the format is backwards compatible with the current implementation. To actually use the new config format, further adjustments are needed, especially in the API. See also: #209, #168, #84.

--- src/config.js
+++ src/config.js
@@ -7,6 +7,33 @@ try {
 }
 let config = Object.assign({}, defaultConfig, userConfig)
 
+// Make new config file format (#64) compatible with current implementation.
+if (config.registries) {
+  config.terminologyProviders = []
+  config.mappingProviders = []
+  config.occurrenceProviders = []
+  for (let registry of config.registries) {
+    if (registry.plugin == "jskos-api") {
+      // baseUrl -> url
+      registry.url = registry.baseUrl
+      // schemes -> voc
+      registry.voc = registry.schemes
+      // Add to list
+      config.terminologyProviders.push(registry)
+    } else if (registry.plugin == "jskos-mappings") {
+      // mappings -> url
+      registry.url = registry.mappings
+      // Add to list
+      config.mappingProviders.push(registry)
+    } else if (registry.plugin == "jskos-occurrences") {
+      // occurrences -> url
+      registry.url = registry.occurrences
+      // Add to list
+      config.occurrenceProviders.push(registry)
+    }
+  }
+}
+
 // load build info into config
 let buildInfo = require("../build/build-info.json")
 config.buildInfo = buildInfo
