ba4787354cddbd91cf1f3f5a9bdb60f6205a3bfe
Stefan Peters <Stefan.Peters@gbv.de>
2018-11-22 09:39:02 +0100

    Adjust config files and handling
    Add a new key "registryMode" which decides whether registries will be merge with ("merge") or overriden by ("override" or empty) the user config. This allows the custom configs for master and dev to become much simpler than before.

--- src/config.js
+++ src/config.js
@@ -7,25 +7,29 @@ try {
 }
 let config = Object.assign({}, defaultConfig, userConfig)
 
+if (config.registryMode == "merge") {
+  config.registries = [].concat(defaultConfig.registries || [], userConfig.registries || [])
+}
+
 // Make new config file format (#64) compatible with current implementation.
 if (config.registries) {
   config.terminologyProviders = []
   config.mappingProviders = []
   config.occurrenceProviders = []
   for (let registry of config.registries) {
-    if (registry.plugin == "jskos-api") {
+    if (registry.plugin == "concept-api") {
       // baseUrl -> url
       registry.url = registry.baseUrl
       // schemes -> voc
       registry.voc = registry.schemes
       // Add to list
       config.terminologyProviders.push(registry)
-    } else if (registry.plugin == "jskos-mappings") {
+    } else if (registry.plugin == "mappings-api") {
       // mappings -> url
       registry.url = registry.mappings
       // Add to list
       config.mappingProviders.push(registry)
-    } else if (registry.plugin == "jskos-occurrences") {
+    } else if (registry.plugin == "occurrences-api") {
       // occurrences -> url
       registry.url = registry.occurrences
       // Add to list
