db6b2cfc72ef71c76772014b59625bddcfbf7a0a
Stefan Peters <Stefan.Peters@gbv.de>
2018-12-10 13:38:03 +0100

    Small adjustment in AutoLink

--- src/components/AutoLink.vue
+++ src/components/AutoLink.vue
@@ -33,7 +33,7 @@ export default {
   methods: {
     // from: https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url (User VicJordan)
     isValidURL: function(string) {
-      return !!string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g)
+      return string && !!string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g)
     }
   }
 }
