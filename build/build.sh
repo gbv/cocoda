b8f96bc5004d1a1836f3d454f03a19bc2914799a
Stefan Peters <Stefan.Peters@gbv.de>
2019-01-17 10:20:24 +0100

    Adjust build script and config files

--- build/build.sh
+++ build/build.sh
@@ -5,23 +5,39 @@ mv build/build-info.json build/build-info.backup.json
 
 # write build info to build-info.js
 GIT_TAG=$(git describe --abbrev=0 --tags || echo "")
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 GIT_COMMIT=$(git rev-parse --verify HEAD)
 GIT_COMMIT_SHORT=$(git rev-parse --verify --short HEAD)
 DATE=$(date)
 cat > ./build/build-info.json <<EOL
 {
   "gitTag": "${GIT_TAG}",
+  "gitBranch": "${GIT_BRANCH}",
   "gitCommit": "${GIT_COMMIT}",
   "gitCommitShort": "${GIT_COMMIT_SHORT}",
   "buildDate": "${DATE}"
 }
 EOL
 
-# make sure a cocoda.json file exists
-[ ! -e ./config/cocoda.json ] && echo -n "{}" > ./config/cocoda.json
+# if there was no config file before, remember this and delete the generated config at the end of the script
+DELETECONFIG=
+[ ! -e ./config/cocoda.json ] && DELETECONFIG=yes
+
+[ -e ./config/cocoda.json ] && echo "Using user config for build..."
+
+# if no user config exists and there is a config for the current branch, use that config
+[ ! -e ./config/cocoda.json ] && [ -e ./config/cocoda.$GIT_BRANCH.json ] && echo "Using config file of branch $GIT_BRANCH for build..." && cp ./config/cocoda.$GIT_BRANCH.json ./config/cocoda.json
+
+# make sure a cocoda.json file exists in all cases
+[ ! -e ./config/cocoda.json ] && echo "Using empty user config for build..." && echo -n "{}" > ./config/cocoda.json
 
 # build the app
 node build/build.js
 
 # move empty build-info.json back after build to not cause a git file change
 mv ./build/build-info.backup.json ./build/build-info.json
+
+# # delete config file if it was generated during this script
+[ $DELETECONFIG ] && echo "Removing config generated during build..." && rm ./config/cocoda.json
+
+exit 0
