7b59c7554cd04b57738cbd28d0bcdc1910c893fd
Stefan Peters <Stefan.Peters@gbv.de>
2019-03-27 11:23:00 +0100

    Fix whitespace issue introduced with vue-cli 3

--- vue.config.js
+++ vue.config.js
@@ -36,5 +36,16 @@ module.exports = {
         }
       ])
     ],
-  }
+  },
+  chainWebpack: config => {
+    // from: https://stackoverflow.com/questions/51304187/spaces-are-gone-in-html-after-upgrading-to-vue-cli-3
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        options.compilerOptions.preserveWhitespace = true
+        return options
+      })
+  },
 }
