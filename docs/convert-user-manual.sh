fc026eba62dbef53f6854e5d08c9e8ce45e7139e
Jakob Voss <voss@gbv.de>
2019-05-02 11:03:12 +0200

    Install more texlive modules on travis-ci

--- docs/convert-user-manual.sh
+++ docs/convert-user-manual.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# exit on failure, run in docs/ directory
+set -e
+cd $(dirname "${BASH_SOURCE[0]}")
+
+PANDOC_MAJOR_VERSION=$(pandoc --version | head -1 | awk '{print int($2)}')
+if [ "$PANDOC_MAJOR_VERSION" -lt 2 ]; then
+    PANDOC_ARGS="--latex-engine=xelatex"
+else
+    PANDOC_ARGS="--pdf-engine=xelatex"
+fi
+
 ./build-user-manual.sh > user-manual.md
 
-pandoc -s -o user-manual.html user-manual.md
-pandoc -o user-manual.pdf user-manual.md
+pandoc $PANDOC_ARGS -s -o user-manual.html user-manual.md
+pandoc $PANDOC_ARGS -o user-manual.pdf user-manual.md
 
 rm user-manual.md
