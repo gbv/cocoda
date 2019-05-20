7ad1d6a01cd56a3d929ba820f9909bb355e19829
Stefan Peters <Stefan.Peters@gbv.de>
2019-05-20 12:43:21 +0200

    Detect invalid config file in build script (#352)

--- build/build.sh
+++ build/build.sh
@@ -23,13 +23,26 @@ EOL
 echo "Building for branch $GIT_BRANCH ($GIT_COMMIT_SHORT)..."
 
 # if there was no config file before, remember this and delete the generated config at the end of the script
-DELETECONFIG=
-[ ! -e ./config/cocoda.json ] && DELETECONFIG=yes
+USERCONFIG=
+MALFORMED_CONFIG=
+BACKUP_FILENAME=./config/cocoda.backup.$(uuidgen).json
 
-[ -e ./config/cocoda.json ] && echo "Using user config for build..."
+if [ -e ./config/cocoda.json ]; then
+  # Check for JSON validity
+  node -e 'var fs = require("fs"); var config = fs.readFileSync("./config/cocoda.json"); try { JSON.parse(config); process.exit(0); } catch(error) { process.exit(1) }'
+  if [ $? -eq 0 ]; then
+    USERCONFIG=yes
+  else
+    echo "Error: User config is not valid JSON! Config file was moved to $BACKUP_FILENAME and falling back to branch or empty user config."
+    mv ./config/cocoda.json $BACKUP_FILENAME
+    MALFORMED_CONFIG=yes
+  fi
+fi
+
+[ $USERCONFIG ] && echo "Using user config for build..."
 
 # if no user config exists and there is a config for the current branch, use that config
-[ ! -e ./config/cocoda.json ] && [ -e ./config/cocoda.$GIT_BRANCH.json ] && echo "Using config file of branch $GIT_BRANCH for build..." && cp ./config/cocoda.$GIT_BRANCH.json ./config/cocoda.json
+[ ! $USERCONFIG ] && [ -e ./config/cocoda.$GIT_BRANCH.json ] && echo "Using config file of branch $GIT_BRANCH for build..." && cp ./config/cocoda.$GIT_BRANCH.json ./config/cocoda.json
 
 # make sure a cocoda.json file exists in all cases
 [ ! -e ./config/cocoda.json ] && echo "Using empty user config for build..." && echo -n "{}" > ./config/cocoda.json
@@ -51,7 +64,10 @@ fi
 mv ./build/build-info.backup.json ./build/build-info.json
 
 # # delete config file if it was generated during this script
-[ $DELETECONFIG ] && echo "Removing config generated during build..." && rm ./config/cocoda.json
+[ ! $USERCONFIG ] && echo "Removing config generated during build..." && rm ./config/cocoda.json
+
+# restore malformed user config
+[ $MALFORMED_CONFIG ] && mv $BACKUP_FILENAME ./config/cocoda.json && echo "Malformed user config was moved back to ./config/cocoda.json."
 
 [ $success -eq 0 ] && echo "Build successfully deployed to folder dist/."
 
