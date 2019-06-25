fb19486d18a36b03760c10e5b5bf3b3f7ab2f5eb
Stefan Peters <Stefan.Peters@gbv.de>
2019-06-25 13:17:13 +0200

    Update build-all script to include GitHub milestones

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -3,6 +3,8 @@
 # Stash changes before running script
 git stash save -u before-build-all
 
+wget https://api.github.com/repos/gbv/cocoda/milestones?state=closed -O github-milestones.json
+
 GIT_BRANCH=master
 
 git checkout $GIT_BRANCH
@@ -26,6 +28,7 @@ do
   GIT_COMMIT_SHORT=$(git rev-parse --verify --short HEAD)
   BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
   DATE=$(node -pe "var date = new Date($(git show -s --format=%ct)*1000); console.log(date); process.exit(0);")
+  MILESTONE_URL=$(node -pe "let ms = require('./github-milestones.json'); let m = ms.find(a => a.title == '$VERSION'); if(m) console.log('\"' + m.html_url + '?closed=1\"'); else console.log('null'); process.exit(0)")
   cat > dist/build-info.json <<EOL
 {
   "version": "${VERSION}",
@@ -33,7 +36,8 @@ do
   "gitCommit": "${GIT_COMMIT}",
   "gitCommitShort": "${GIT_COMMIT_SHORT}",
   "buildDate": "${BUILD_DATE}",
-  "date": "${DATE}"
+  "date": "${DATE}",
+  "milestoneUrl": ${MILESTONE_URL}
 }
 EOL
   # 6. Move build to separate folder
@@ -42,6 +46,7 @@ EOL
   git reset --hard
 done
 
+rm github-milestones.json
 git checkout dev
 
 # Apply stash after script
