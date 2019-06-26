d25c57fab75b2eb4c11fdeeea944e96dbda3d048
Jakob Voss <voss@gbv.de>
2019-06-26 09:27:02 +0200

    Start German user manual

--- docs/build-user-manual.sh
+++ docs/build-user-manual.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-./build-user-manual.sh > user-manual.md
+./combine-user-manual.sh > user-manual.md
 
+# tested with Pandoc 2.2.1
 pandoc -s -o user-manual.html user-manual.md
-pandoc -o user-manual.pdf user-manual.md
+pandoc -o user-manual.pdf user-manual.md --template template.tex
 
 rm user-manual.md
---
+++ docs/de/cocoda-anleitung.md
@@ -0,0 +1,5 @@
+---
+title: Cocoda-Anleitung
+...
+
+TODO
