e02bcb78930f4d48c1722bd79ecefe8b5d69802a
Jakob Voss <voss@gbv.de>
2019-06-26 09:05:33 +0200

    Rename user-manual scripts

--- docs/build-user-manual.sh
+++ docs/build-user-manual.sh
@@ -1,47 +1,8 @@
 #!/bin/bash
 
-echo "---"
-echo title: 'Cocoda User Manual'
-echo author: 
-echo date: $(date +%F)
-echo "..."
+./build-user-manual.sh > user-manual.md
 
-echo
-echo 
-echo "## Introduction"
-cat guide.md
+pandoc -s -o user-manual.html user-manual.md
+pandoc -o user-manual.pdf user-manual.md
 
-echo
-echo 
-echo "## User Interface"
-cat guide-interface.md
-
-echo
-echo
-echo "## Login"
-cat guide-login.md
-
-echo
-echo "## Concept Schemes"
-cat guide-concept-schemes.md
-
-echo
-echo
-echo "## Concepts"
-cat guide-concepts.md
-
-echo
-echo
-echo "## Mappings"
-cat guide-mappings.md
-
-echo
-echo
-echo "## Registries"
-cat registries.md
-
-echo
-echo
-echo "## Settings"
-cat guide-settings.md
-
+rm user-manual.md
