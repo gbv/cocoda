131b95462c7d53d175d45d3ac4ea4033b54a46f9
Stefan Peters <Stefan.Peters@gbv.de>
2019-09-12 08:27:57 +0200

    Revert "SkosmosApi: Add notations"
    This reverts commit e20d635f85e3004a6dfadc9714502e6bb76de344.
    Reason: This commit was added to work around a bug with notations. This bug happens to have been caused in the objects mixin and was fixed in 754c703d072c41e5b8e043a39ca89754aad5ea6d.

--- src/providers/skosmos-api-provider.js
+++ src/providers/skosmos-api-provider.js
@@ -51,16 +51,6 @@ class SkosmosApiProvider extends BaseProvider {
     return []
   }
 
-  addNotationToConcept(concept, scheme) {
-    if (!scheme || !scheme.uriPattern || !concept || !concept.uri || concept.notation) {
-      return
-    }
-    const notation = util.notation({ uri: concept.uri, inScheme: [scheme] })
-    if (notation) {
-      concept.notation = [notation]
-    }
-  }
-
   getDataUrl(concept, { addFormatParameter = true } = {}) {
     const scheme = _.get(concept, "inScheme[0]")
     if (!concept.uri || !scheme || !scheme.VOCID) {
@@ -119,8 +109,6 @@ class SkosmosApiProvider extends BaseProvider {
             }
             // Set ancestors to empty array
             relative.ancestors = []
-            // Set notation of relative
-            this.addNotationToConcept(relative, _.get(concept, "inScheme[0]"))
           }
         }
         // ESLint exceptions see: https://github.com/eslint/eslint/issues/11899
@@ -146,8 +134,6 @@ class SkosmosApiProvider extends BaseProvider {
         }
         // eslint-disable-next-line require-atomic-updates
         concept.type = _.uniq(concept.type)
-        // Add notation
-        this.addNotationToConcept(concept, _.get(concept, "inScheme[0]"))
         newConcepts.push(concept)
       }
     }
@@ -197,6 +183,7 @@ class SkosmosApiProvider extends BaseProvider {
     }
     const concepts = []
     for (let concept of response.results || []) {
+      const notation = util.notation({ uri: concept.uri, inScheme: [scheme] })
       const label = concept.matchedPrefLabel || concept.altLabel || concept.prefLabel
       const newConcept = {
         uri: concept.uri,
@@ -205,7 +192,9 @@ class SkosmosApiProvider extends BaseProvider {
         },
         inScheme: [scheme],
       }
-      this.addNotationToConcept(newConcept, scheme)
+      if (notation) {
+        newConcept.notation = [notation]
+      }
       concepts.push(newConcept)
     }
     return concepts
