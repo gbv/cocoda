06433062f89672ec618edff734e036f445820eb0
Stefan Peters <Stefan.Peters@gbv.de>
2019-09-30 09:24:28 +0200

    i18n: Silent fallback warnings when not using dev server

--- src/util/i18n.js
+++ src/util/i18n.js
@@ -4,9 +4,12 @@ import VueI18n from "vue-i18n"
 Vue.use(VueI18n)
 import messages from "../../config/locale.json"
 
+
 const i18n = new VueI18n({
   locale: "en",
   fallbackLocale: "en",
+  // Silent fallback warnings when not using dev server
+  silentFallbackWarn: !window.webpackHotUpdate,
   messages,
 })
 
