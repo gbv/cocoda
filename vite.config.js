6587dab7c897b24f5bc3fc229639bea58ef84b80
Stefan Peters <stefandesu@exo.pm>
2021-10-13 13:15:09 +0200

    Use symlinks for files in public/ instead of copying

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig } from "vite"
 import { resolve } from "path"
 import { createVuePlugin as vue } from "vite-plugin-vue2"
 
-// Copy static files to public folder
-// TODO: Is there a better way of doing this?
-import fs from "fs"
-fs.copyFileSync("build/build-info.json", "public/build-info.json")
-fs.copyFileSync("config/cocoda.json", "public/cocoda.json")
-if (!fs.existsSync("public/css")) {
-  fs.mkdirSync("public/css")
-}
-fs.copyFileSync("node_modules/bootstrap/dist/css/bootstrap.css", "public/css/bootstrap.css")
-fs.copyFileSync("node_modules/bootstrap-vue/dist/bootstrap-vue.css", "public/css/bootstrap-vue.css")
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
