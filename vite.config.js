5e1a31b0a058cb5b323a255d525ece2b681c2337
Stefan Peters <Stefan.Peters@gbv.de>
2022-02-10 12:31:10 +0100

    Add rollup-plugin-visualizer to visualize bundle stats

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
 import { createVuePlugin as vue } from "vite-plugin-vue2"
+import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [
     vue(),
+    visualizer(),
   ],
   build: {
     rollupOptions: {
