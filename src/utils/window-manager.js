9991ea017cb4d3857438bb731096319451e48b71
Stefan Peters <Stefan.Peters@gbv.de>
2022-02-17 12:02:20 +0100

    Adjustments to window manager
    - Don't save window/eventType if eventType is not given.
    - Add "name" parameter.

--- src/utils/window-manager.js
+++ src/utils/window-manager.js
@@ -2,9 +2,12 @@ const windowManager = {
   window: null,
   eventType: null,
 }
-export function openWindow({ url, eventType }) {
-  windowManager.window = window.open(url)
-  windowManager.eventType = eventType
+export function openWindow({ url, eventType, name }) {
+  const newWindow = window.open(url, name)
+  if (eventType) {
+    windowManager.window = newWindow
+    windowManager.eventType = eventType
+  }
 }
 export function closeWindow({ eventType } = {}) {
   if (windowManager.window && windowManager.eventType == eventType) {
